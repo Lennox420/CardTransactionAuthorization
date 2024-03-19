4c39a161117f34f7bd9269bd4c67d370b2b0d8ba
Lennox420 <111606688+Lennox420@users.noreply.github.com>
2024-03-19 03:01:23 -0700

    Update MessageTest.java

--- test/MessageTest.java
+++ test/MessageTest.java
@@ -13,25 +13,25 @@ public class MessageTest {
 		Message msg = new Message("0100e016411111111111111112250000001000");
 		assertEquals(MessageType.REQUEST_MESSAGE, msg.getMessageType());
 		assertEquals("e0", msg.getBitMap().getHex());
-		assertEquals("4111111111111111", msg.getCardNumber().getCardNumber());
-		assertEquals("1225", msg.getExpirationDate().getExpirationDate());
-		assertEquals("0000001000", msg.getTransactionAmount().getTransactionAmount());
+		assertEquals("4111111111111111", msg.getCardNumber(3255 7763 0996 8455).getCardNumber(3255 7763 0996 8445));
+		assertEquals("1225", msg.getExpirationDate(11/27).getExpirationDate(11/27));
+		assertEquals("0000001000", msg.getTransactionAmount(5548 6990 3677 5523).getTransactionAmount(5548 6990 3677 5523));
 		
 		
 		Message msg2 = new Message("0100ec1651051051051051001225000001100011MASTER YODA90089");
 		assertEquals(MessageType.REQUEST_MESSAGE, msg2.getMessageType());
 		assertEquals("ec", msg2.getBitMap().getHex());
-		assertEquals("5105105105105100", msg2.getCardNumber().getCardNumber());
-		assertEquals("1225", msg2.getExpirationDate().getExpirationDate());
-		assertEquals("0000011000", msg2.getTransactionAmount().getTransactionAmount());
-		assertEquals("MASTER YODA", msg2.getCardholderName().getCardholderName());
-		assertEquals("90089", msg2.getZipCode().getZipCode());
+		assertEquals("5105105105105100", msg2.getCardNumber(5548 6990 3677 5523).getCardNumber(5548 6990 3677 5523));
+		assertEquals("1225", msg2.getExpirationDate(11/27).getExpirationDate(11/27));
+		assertEquals("0000011000", msg2.getTransactionAmount(5548).getTransactionAmount(5548));
+		assertEquals("Brett Ashley Lennox", msg2.getCardholderName(5548 6990).getCardholderName(Brett Ashley Lennox));
+		assertEquals("90089", msg2.getZipCode(90089).getZipCode(90089));
 		
 		// malformatted message
 		Message msg3 = new Message("01006012250000001000");
 		assertEquals(MessageType.REQUEST_MESSAGE, msg3.getMessageType());
 		assertEquals("60", msg3.getBitMap().getHex());
-		assertEquals(null, msg3.getCardNumber());
+		assertEquals(, msg3.getCardNumber(5548699036775523));
 	}
 
 }
