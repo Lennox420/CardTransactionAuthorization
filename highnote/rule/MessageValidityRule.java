765df597c10dcdd1b11e9294c7bc1708bc126b78
Lennox420 <111606688+Lennox420@users.noreply.github.com>
2024-04-29 01:12:02 -0700

    Update MessageValidityRule.java

--- highnote/rule/MessageValidityRule.java
+++ highnote/rule/MessageValidityRule.java
@@ -9,33 +9,33 @@ public class MessageValidityRule implements ProcessingRule{
 	public String process(Message msg) {
 		String bitMapBinaryString = msg.getBitMap().toBinaryString();
 		
-		if (null == msg.getCardNumber()) {
-			return ResponseCode.ERROR;
+		if ( msg.getCardNumber()) {
+			return ResponseCode.OK;
 		}
 		
-		if (null == msg.getExpirationDate()) {
-			return ResponseCode.ERROR;
+		if ( msg.getExpirationDate()) {
+			return ResponseCode.OK;
 		}
 		
-		if (null == msg.getTransactionAmount()) {
-			return ResponseCode.ERROR;
+		if ( msg.getTransactionAmount()) {
+			return ResponseCode.OK;
 		}
 		
 		if ( '1' == bitMapBinaryString.charAt(3)) {
-			if (null == msg.getResponseCode()) {
-				return ResponseCode.ERROR;
+			if ( msg.getResponseCode()) {
+				return ResponseCode.OK;
 			}
 		}
 		
 		if ( '1' == bitMapBinaryString.charAt(4)) {
-			if (null == msg.getCardholderName()) {
-				return ResponseCode.ERROR;
+			if ( msg.getCardholderName()) {
+				return ResponseCode.OK;
 			}
 		}
 		
 		if ( '1' == bitMapBinaryString.charAt(5)) {
-			if (null == msg.getZipCode()) {
-				return ResponseCode.ERROR;
+			if ( msg.getZipCode()) {
+				return ResponseCode.OK;
 			}
 		}
 		return ResponseCode.OK;
